c71b2cf072363c3c697ec2c4228217692e00122f
Foshati <amirrezafoshati@gmail.com>
2024-05-27 23:27:13 +0330

    add style and design productDetails

--- src/app/(front)/product/[slug]/page.tsx
+++ src/app/(front)/product/[slug]/page.tsx
@@ -86,48 +86,13 @@ export default function ProductDetails({ params, }: { params: { slug: string } }
                                         >
                                             <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" />
                                         </svg>
-                                        <span className="text-gray-600 ml-3">4 Reviews</span>
+                                        <span className="text-gray-600 ml-3">{productDetails.rating}</span>
                                     </span>
                                     <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
-                                        <a className="text-gray-500">
-                                            <svg
-                                                fill="currentColor"
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                strokeWidth={2}
-                                                className="w-5 h-5"
-                                                viewBox="0 0 24 24"
-                                            >
-                                                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
-                                            </svg>
-                                        </a>
-                                        <a className="text-gray-500">
-                                            <svg
-                                                fill="currentColor"
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                strokeWidth={2}
-                                                className="w-5 h-5"
-                                                viewBox="0 0 24 24"
-                                            >
-                                                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
-                                            </svg>
-                                        </a>
-                                        <a className="text-gray-500">
-                                            <svg
-                                                fill="currentColor"
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                strokeWidth={2}
-                                                className="w-5 h-5"
-                                                viewBox="0 0 24 24"
-                                            >
-                                                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
-                                            </svg>
-                                        </a>
+                                        <span className="text-slate-600" >Number of products:{productDetails.countInStock}</span>
                                     </span>
                                 </div>
-                                <p className="leading-relaxed">
+                                <p className="leading-relaxed text-slate-400">
                                     {productDetails.description2}
                                 </p>
                                 <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
@@ -139,13 +104,14 @@ export default function ProductDetails({ params, }: { params: { slug: string } }
                                     </div>
                                     <div className="flex ml-6 items-center">
                                         <span className="mr-3">Size</span>
-                                        <div className="relative">
+                                        <div className="relative ">
                                             <select className="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
                                                 <option>SM</option>
                                                 <option>M</option>
                                                 <option>L</option>
                                                 <option>XL</option>
                                             </select>
+
                                             <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
                                                 <svg
                                                     fill="none"
@@ -167,7 +133,7 @@ export default function ProductDetails({ params, }: { params: { slug: string } }
                                         {productDetails.price}$
                                     </span>
                                     <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
-                                        Button
+                                        Add tScoreo Cart
                                     </button>
                                     <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                                         <svg
